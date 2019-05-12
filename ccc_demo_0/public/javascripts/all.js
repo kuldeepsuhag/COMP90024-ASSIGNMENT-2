71b609ae682f2d386232f5e40a1135ba05dd8a1e
SinkableFish <nihongtao96@gmail.com>
2019-05-12 23:24:15 +1000

    update method to gain ip address

--- ccc_demo_0/public/javascripts/all.js
+++ ccc_demo_0/public/javascripts/all.js
@@ -6,21 +6,24 @@ var inside = require('point-in-polygon');
 var fs = require('fs');
 var os = require('os');
 
-var ipaddress;
-var ifaces = os.networkInterfaces();
-Object.keys(ifaces).forEach(function (ifname) {
-  var alias = 0;
-  ifaces[ifname].forEach(function (iface) {
-    if ('IPv4' !== iface.family || iface.internal !== false) {
-      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
-      return;
-    }
-    if(ifname == "eth0" || ifname == 'en0'){
-        ipaddress = iface.address;
-        console.log(ipaddress);
-    }
-  });
-});
+var ipFile = fs.readFileSync('../../ipAddress.txt');
+var allIp = ipFile.toString();
+var ips = allIp.split(" ");
+var ipaddress = ips[0];
+// var ifaces = os.networkInterfaces();
+// Object.keys(ifaces).forEach(function (ifname) {
+//   var alias = 0;
+//   ifaces[ifname].forEach(function (iface) {
+//     if ('IPv4' !== iface.family || iface.internal !== false) {
+//       // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+//       return;
+//     }
+//     if(ifname == "eth0" || ifname == 'en0'){
+//         ipaddress = iface.address;
+//         console.log(ipaddress);
+//     }
+//   });
+// });
 
 
 const couch = new NodeCouchDb({
